9129510a857c9e504592ecc40eec5ee1429c52da
= <=>
2022-04-06 12:39:43 +0700

    'pertemuan6'

---
+++ resources/views/home.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+
+                    <table class="table table-responsive">
+                        <tr><th>Username</th><tr>:</th><td>{{ $user->username }}</td></tr>
+                        <tr><th>Name</th><tr>:</th><td>{{ $user->name }}</td></tr>
+                        <tr><th>Email</th><tr>:</th><td>{{ $user->email }}</td></tr>
+                        <tr><th>Created At</th><tr>:</th><td>{{ $user->create_at }}</td></tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
